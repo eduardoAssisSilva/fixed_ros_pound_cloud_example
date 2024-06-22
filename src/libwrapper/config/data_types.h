1a6ee1feb85359c3f1b789ee4ba68783a96c0a2c
eduardoAssisSilva <eduardoassis52@hotmail.com>
2024-06-22 15:41:30 -0300

    TesteTalker

--- src/libwrapper/config/data_types.h
+++ src/libwrapper/config/data_types.h
@@ -1,8 +1,12 @@
 /* Examples
 #include "std_msgs/Float64.h"
 #include <std_msgs/Int32.h>
-*/
 
 #include <turtlesim/Pose.h>
 #include <geometry_msgs/Twist.h>
+*/
+
+#include <std_msgs/String.h>
+
+
 
